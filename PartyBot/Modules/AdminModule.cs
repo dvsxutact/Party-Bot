20398c17d624364a7ea0e1a586aca3d21eb06240
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-04 02:07:32 +0000

    Update AdminModule.cs

--- PartyBot/Modules/AdminModule.cs
+++ PartyBot/Modules/AdminModule.cs
@@ -14,27 +14,5 @@ namespace PartyBot.Modules
         [Command("Info")]
         public async Task Info()
             => await ReplyAsync("", false, await BotService.DisplayInfoAsync(Context));
-
-        [Command("test"), RequireUserPermissionAttribute(GuildPermission.Administrator)]
-        public async Task Prefix()
-        {
-            var img = Context.User.GetAvatarUrl();
-            var defaultImg = Context.User.GetDefaultAvatarUrl();
-            if(img != null)
-                await ReplyAsync($"{img}");
-            else
-                await ReplyAsync($"{defaultImg}");
-        }
-
-
-        public void SwitchA()
-        {
-
-        }
-
-        public void SwitchB()
-        {
-
-        }
     }
 }
